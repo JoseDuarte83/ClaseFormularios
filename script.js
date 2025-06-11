323db3f80064a78c05891faa7253a28d82e1d4de
JoseDuarte83 <joseduartesls@hotmail.com>
2025-06-10 18:54:39 -0600

    se termina y pasa a prueba

--- script.js
+++ script.js
@@ -46,6 +46,38 @@ document.addEventListener('DOMContentLoaded',()=>{
             mostrarError(errorNombre,nombreInput, 'El nombre es requerido');
             formValido = false;
         }
+        // validación de email
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (emailInput.value.trim() === ''){
+            mostrarError(errorEmail,emailInput, 'El correo es requerido');
+        }else if (!emailPattern.test(emailInput.value.trim())){
+            mostrarError(errorEmail,emailInput,'Ingrese un correo válido.');
+            formValido = false
+        }
+        //Validación de contraseña
+        if (passwordInput.value.trim() === ''){
+            mostrarError(errorPassword,passwordInput,'La contraseña es requerida');
+            formValido = false;
+        }else if (passwordInput.value.trim().lenght <6){
+            mostrarError(errorPassword,passwordInput,'La contraseña debe ser mayor a seis caracteres');
+            formValido = false;
+        }
+        //validación de confirmación de contraseña
+        if (confirmPasswordInput.value.trim() === ''){
+            mostrarError(errorConfirmPassword,confirmPasswordInput, 'Debe de reingresar la contraseña');
+            formValido = false;
+        }else if (confirmPasswordInput.value !== passwordInput.value){
+            mostrarError(errorConfirmPassword,confirmPasswordInput, 'Las contraseñas no coinciden');
+            formValido = false;
+        }
+    
+    //si el formulario es válido, mostrar el mensaje de éxito y resetar form
+
+    if (formValido) {
+        successMessage.style.display = 'block';
+        registroForm.reset(); // limpia todos los campos del form
+        console.log('Formulario enviado correctamente')
+    }
 
 
     })
